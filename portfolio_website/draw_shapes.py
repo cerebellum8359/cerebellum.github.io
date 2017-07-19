d07f9fd8647283c30ba0a80c42bda4f9b7f84a3d
Abigail Cumberbatch <cumberbatchabigail@gmail.com>
2017-07-19 13:03:37 -0400

    changing folder structure

---
+++ portfolio_website/draw_shapes.py
@@ -0,0 +1,28 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+Harold = Turtle()
+# Set Up your screen and starting position.
+setup(500, 500)
+x_pos = -250
+y_pos = -150
+Harold.setposition( 250,150)
+# Write your code below
+pencolor("240")
+right( 90)
+forward(70)
+right (90)
+forward(70)
+right (90)
+forward (70)
+right (90)
+forward (70)
+
+
+
+
+
+
+# Close window on click.
+exitonclick()
