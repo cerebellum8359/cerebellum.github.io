c9d9513634ebe3b58e78c06d6770d0c10d77b872
Abigail Cumberbatch <cumberbatchabigail@gmail.com>
2017-07-24 08:55:33 -0400

    Updated folder

---
+++ Python - Copy/grades.py
@@ -0,0 +1,46 @@
+#from numpy import
+
+# Each of our grades
+kristen_grades = [90, 85, 100, 77, 94]
+clarisse_grades = [96, 83, 89, 97, 86]
+sapna_grades = [82, 91, 94, 87, 99]
+
+# Class grade book
+grade_book = [kristen_grades, clarisse_grades, sapna_grades]
+
+'''
+This is what our grade book looks like
+[	[90, 85, 100, 77, 94]
+	[96, 83, 89, 97, 86]
+	[82, 91, 94, 87, 99] ]
+
+'''
+
+# Traverse through the grade book and print all the indivdual grades
+for g in grade_book:
+	for n in g:
+		print (n)
+
+
+# Extensions: Find the class average, median, and standard deviation
+# (For the extentions google for hints!)
+sum(kristen_grades)
+len(kristen_grades)
+sum(kristen_grades)/len(kristen_grades)
+print(sum(kristen_grades)/ len(kristen_grades))
+sum(clarisse_grades)
+len(clarisse_grades)
+sum(clarisse_grades)/len(clarisse_grades)
+print(sum(clarisse_grades))/len(clarisse_grades)
+sum(sapna_grades)
+len(sapna_grades)
+sum(sapna_grades)/ len(sapna_grades)
+print(sum(sapna_grades))/ len(sapna_grades)
+Averages = [89,90,90]
+sum(Averages)
+len(Averages)
+sum(Averages)/len(Averages)
+print(sum(Averages))/len(Averages)
+
+
+# Super extra extensions: calculate the student with highest/lowest average
--- Python - Copy/obamacon.py
+++ Python - Copy/obamacon.py
@@ -10,23 +10,30 @@ yellow = (252, 227, 166)
 # change IMAGENAME to the path on your computer to the image 
 # you're using'''
 
-my_image = Image.open("IMAGENAME") 
+my_image = Image.open("brain.jpg") 
 
 # each pixel is represented in the form 
 # (red value, green value, blue value, transparency)
 image_list = my_image.getdata()  
 
 # Turns the sequence above into a list, it can be iterated through
-# in a loop
-image_list = list(image_list)
+
+image_list = list[image_list]
 
 # list that will hold the pixel data for the new image
 recolored = [] 
 #**************************************************#
 #**************************************************#
+for x in image_list:
+	image_list = [(400,200,50),(300,150,30), (200,100,20)]
+	intensity = x[0] + x[1] + x[2]
+	if intensity > 546:
+		recolored.append(yellow)
+	elif intensity >= 364 and =< 546:
+		recolored.append(lightBlue)
+	elif intensity >= 182 and =< 364:
+		recolored.append(red)
 
-#YOUR CODE to loop through the original list of pixels and 
-# build a new list based on intensity should go here.
 
 
 
