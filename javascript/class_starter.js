411c9ffa6f1137b8bb9f1f94e8f2507240744d24
Abigail Cumberbatch <cumberbatchabigail@gmail.com>
2017-08-04 15:13:14 -0400

    updated folder

--- javascript/class_starter.js
+++ javascript/class_starter.js
@@ -5,16 +5,16 @@ class person {
 	this. address = address;
 }
     getFullName(){
-	 document.getElementById("myName").innerHTML = this.first_name + this.last_name;
+	 document.getElementById("myName").innerText = this.first_name + this.last_name;
 	}
 	 	
 	getAddress() {
-		document.getElementById("myAddress").innerHTML = this.address;
+		document.getElementById("myAddress").innerText = this.address;
 
 	}
 
 }
 
-var tom = new person ( "Tom ", "Cruise", "Beverly Hills")
+var tom = new person ( "Tom ", "Cruise", "Beverly Hills");
 tom.getFullName ();
 tom.getAddress();
